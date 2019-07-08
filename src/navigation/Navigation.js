127939eeb3c9a23a4fcc85f483e5db15eb9e247a
Steven <48270398+Lee2se@users.noreply.github.com>
2019-07-08 19:00:45 -0400

    Added Nav Bar

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import SignOut from '../auth/SignOut';
+
+export default class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand></NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle className="mr-3" nav caret>
+                            Pages
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                            <DropdownItem tag={Link} to='/'>
+                                Home
+                            </DropdownItem>
+                            <DropdownItem tag={Link} to='/search'>
+                                Search
+                            </DropdownItem>
+                            <DropdownItem tag={Link} to='/upload'>
+                                Upload
+                            </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <NavItem>
+                            <SignOut />
+                        </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
