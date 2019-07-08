e7ffef04fe51c733b1433ec6433678c880ca9da2
Steven <48270398+Lee2se@users.noreply.github.com>
2019-07-08 18:56:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Amplify from 'aws-amplify';
 import { SignIn, ConfirmSignIn, ForgotPassword, RequireNewPassword, VerifyContact, withAuthenticator } from 'aws-amplify-react';
 import aws_exports from './aws-exports';
 import './App.css';
-import SignOut from './auth/SignOut';
+import Navigation from './navigation/Navigation';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <SignOut />
-        
-      </React.Fragment>
+      <Router>
+        <Fragment>
+          <Navigation />  
+        </Fragment>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='search' component={Search} />
+        </Switch>
+      </Router>
     );
   }
 }
