a7b4dea784f8eaab7739858d20d2d69c81738165
Steven <48270398+Lee2se@users.noreply.github.com>
2019-07-09 12:27:49 -0400

    Minor fix

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Input, } from 'reactstrap';
+import { Button, FormGroup, Input } from 'reactstrap';
 
 export default class Search extends Component {
     render() {
