641e112945921b2dc7796107a0b104f64f40adb2
Steven <lee25se@gmail.com>
2019-07-09 12:23:53 -0400

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Input } from 'reactstrap';
+import { Button, FormGroup, Input, } from 'reactstrap';
 
 export default class Search extends Component {
     render() {
         return (
-            <div>
-             <form>
+            <div className="text-center mt-4 row">
+             <form className="col-md-4 mx-auto">
                 <FormGroup>
-                    <Input type="text" id="text" name="text" required />
+                    <Input type="text" className="form-control" id="text" name="text" required />
                 </FormGroup>
-                <Button className="mx-auto" color="primary">Submit</Button>
+                <Button color="primary">Search</Button>
             </form>   
             </div>
         )
