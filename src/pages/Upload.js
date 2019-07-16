450b524095dcd08d0050c41720b5ab03401684f1
Jason Brito <jason.brito@evanschambers.com>
2019-07-16 13:47:30 -0400

    Implement drag&drop

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -18,8 +18,8 @@ export default class Upload extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            file: '', // data of file in base64
-            filename: '', // name of file
+            file: '', // data of file to be uploaded to S3 bucket
+            filename: '', // name of file to be uploaded to S3 bucket
             files: [] // files in S3 bucket
         };
     }
@@ -27,30 +27,21 @@ export default class Upload extends Component {
     // load files when component mounts
     async componentDidMount() { this.getFiles(); };
 
-    // get new file and set state with new filename
-    setFile = change => {
-        const file = change.target.files[0];
-        if (!file) { return; }
-        this.getBase64(file);
+    // read new file as base64 and set state with new file data
+    saveFile = file => {
         this.setState({ filename: file.name });
-    };
-
-    // read new file as base64 and set state with new file
-    getBase64 = file => {
         const fileReader = new FileReader();
         fileReader.readAsDataURL(file);
-        fileReader.onload = () => { this.state.file = this.formatBase64(fileReader.result); };
-    };
-
-    // remove extraneous header from base64
-    formatBase64 = result => {
-        const base64 = result.split(',');
-        base64.shift();
-        return base64.join('');
+        fileReader.onload = () => {
+            let base64 = fileReader.result.split(',');
+            base64.shift();
+            this.setState({ file: base64.join('') });
+            this.putFile();
+        };
     };
 
     // call api to PUT file then reload files
-    saveFile = () => {
+    putFile = () => {
         const params = {
             headers: { "Access-Control-Allow-Origin": "*" },
             body: { file: this.state.file, filename: this.state.filename }
@@ -82,13 +73,13 @@ export default class Upload extends Component {
     formatSize = B => {
         switch (true) {
             case (B >= 1000 && B < 1000000) :
-                return (B / 1024).toFixed(2) + ' KB';
+                return (B / 1024).toFixed(2) + " KB";
                 break;
             case (B >= 1000000) :
-                return (B / 1e+6).toFixed(2) + ' MB';
+                return (B / 1e+6).toFixed(2) + " MB";
                 break;
             default :
-                return B.toFixed(2) + ' B';
+                return B.toFixed(2) + " B";
         }
     };
 
@@ -110,7 +101,7 @@ export default class Upload extends Component {
                                 return(
                                     <tr key={ index }>
                                         <td>{ file.Key }</td>
-                                        <td>{ file.LastModified.substring(0, file.LastModified.indexOf('T')) }</td>
+                                        <td>{ file.LastModified.substring(0, file.LastModified.indexOf("T")) }</td>
                                         <td>{ this.formatSize(file.Size) }</td>
                                     </tr>
                                 );
@@ -122,8 +113,9 @@ export default class Upload extends Component {
         );
     };
 
+    // render dropzone for drag&drop upload
     renderDropzone = () => {
-        const handleSubmit = (files, all) => { files.map(file => { console.log(file); file.remove(); }) }
+        const handleSubmit = files => { files.map(file => { this.saveFile(file.file); file.remove(); }); };
         const getInputContent = () => {
             return(
                 <div className="input-content">
@@ -131,13 +123,14 @@ export default class Upload extends Component {
                     <p className="lead"><strong>Upload</strong></p>
                 </div>
             );
-        }
+        };
         return(
             <Dropzone
-                inputContent = { getInputContent() }
-                inputWithFilesContent =  { <FontAwesomeIcon icon="cloud-upload-alt" size="lg"/> }
-                onSubmit = { handleSubmit }
-                styles = { { dropzone: { minHeight: 300 } } }
+                inputContent={ getInputContent() }
+                inputWithFilesContent={ <FontAwesomeIcon icon="cloud-upload-alt" size="lg"/> }
+                submitButtonContent="Upload"
+                onSubmit={ handleSubmit }
+                styles={ { dropzone: { minHeight: 300 } } }
             />
         );
     };
@@ -147,12 +140,7 @@ export default class Upload extends Component {
         return(
             <Card>
                 <CardHeader>Upload</CardHeader>
-                <CardBody>
-                    <Input type="file" onChange={ this.setFile } />
-                    <br/>
-                    <p className="lead">{ this.state.filename }</p>
-                    { this.renderDropzone() }
-                </CardBody>
+                <CardBody>{ this.renderDropzone() }</CardBody>
                 <CardFooter><Button outline color="secondary" onClick={ this.saveFile }>upload</Button></CardFooter>
             </Card>
         );
