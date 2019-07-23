0ffa31c46ced26b92c7c25e6893b51ffef4a1faa
Jason Brito <jason.brito@evanschambers.com>
2019-07-23 15:39:27 -0400

    Connect React to Elasticsearch

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, Input, Alert  } from 'reactstrap';
 
+import { API } from 'aws-amplify';
+
+const api = 'filestorageapi';
+const basePath = '/es';
 
 export default class Search extends Component {
     state = {
         text: '',
-        alert: null
+        alert: null,
     };
 
+    query = () => {
+        if (!this.state.text.length) { return }
+        const params = { headers: { "Access-Control-Allow-Origin": "*" } };
+        const qsp = "?q=";
+        const path = basePath + qsp + this.state.text;
+        API.get(api, path, params)
+            .then(res => {
+                console.log("results loaded successfully!", res);
+                console.log(res.hits.hits[0])
+            })
+            .catch(err => {
+                console.log("results failed to load.", err);
+            });
+    };
 
+    handleChange = change => { this.setState({ text: change.target.value }); }
 
     render() {
         return (
@@ -18,10 +37,12 @@ export default class Search extends Component {
             </Alert> */}
              <form className="col-md-4 mx-auto">
                 <FormGroup>
-                    <Input type="text" className="form-control" id="text" name="text" required />
+                    <Input type="text" className="form-control" id="text" name="text" required
+                        onChange={this.handleChange} />
                 </FormGroup>
-                <Button color="primary">Search</Button>
-            </form>   
+                <Button color="primary" onClick={this.query}>Search</Button>
+            </form>
+            { this.state.results }
             </div>
         )
     }
